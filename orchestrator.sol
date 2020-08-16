be06af11b654de808ac64b71de44925fc779c79e
satoshinate <69215840+satoshinate@users.noreply.github.com>
2020-08-16 04:42:19 +0000

    Create orchestrator.sol

---
+++ orchestrator.sol
@@ -0,0 +1,121 @@
+pragma solidity >=0.4.24;
+
+import "openzeppelin-eth/contracts/ownership/Ownable.sol";
+import "./dollarsPolicy.sol";
+
+/*
+ *  Orchestrator
+ */
+
+
+contract Orchestrator is Ownable {
+
+    struct Transaction {
+        bool enabled;
+        address destination;
+        bytes data;
+    }
+
+    event TransactionFailed(address indexed destination, uint index, bytes data);
+
+    Transaction[] public transactions;
+
+    DollarsPolicy public policy;
+
+    constructor(address policy_) public {
+        Ownable.initialize(msg.sender);
+        policy = DollarsPolicy(policy_);
+    }
+
+    function rebase()
+        external
+    {
+        require(msg.sender == tx.origin);  // solhint-disable-line avoid-tx-origin
+
+        policy.rebase();
+
+        for (uint i = 0; i < transactions.length; i++) {
+            Transaction storage t = transactions[i];
+            if (t.enabled) {
+                bool result =
+                    externalCall(t.destination, t.data);
+                if (!result) {
+                    emit TransactionFailed(t.destination, i, t.data);
+                    revert("Transaction Failed");
+                }
+            }
+        }
+    }
+
+    function addTransaction(address destination, bytes data)
+        external
+        onlyOwner
+    {
+        transactions.push(Transaction({
+            enabled: true,
+            destination: destination,
+            data: data
+        }));
+    }
+
+    function removeTransaction(uint index)
+        external
+        onlyOwner
+    {
+        require(index < transactions.length, "index out of bounds");
+
+        if (index < transactions.length - 1) {
+            transactions[index] = transactions[transactions.length - 1];
+        }
+
+        transactions.length--;
+    }
+
+    function setTransactionEnabled(uint index, bool enabled)
+        external
+        onlyOwner
+    {
+        require(index < transactions.length, "index must be in range of stored tx list");
+        transactions[index].enabled = enabled;
+    }
+
+    function transactionsSize()
+        external
+        view
+        returns (uint256)
+    {
+        return transactions.length;
+    }
+
+    function externalCall(address destination, bytes data)
+        internal
+        returns (bool)
+    {
+        bool result;
+        assembly {  // solhint-disable-line no-inline-assembly
+            // "Allocate" memory for output
+            // (0x40 is where "free memory" pointer is stored by convention)
+            let outputAddress := mload(0x40)
+
+            // First 32 bytes are the padded length of data, so exclude that
+            let dataAddress := add(data, 32)
+
+            result := call(
+                // 34710 is the value that solidity is currently emitting
+                // It includes callGas (700) + callVeryLow (3, to pay for SUB)
+                // + callValueTransferGas (9000) + callNewAccountGas
+                // (25000, in case the destination address does not exist and needs creating)
+                sub(gas, 34710),
+
+
+                destination,
+                0, // transfer value in wei
+                dataAddress,
+                mload(data),  // Size of the input, in bytes. Stored in position 0 of the array.
+                outputAddress,
+                0  // Output is ignored, therefore the output size is zero
+            )
+        }
+        return result;
+    }
+}
